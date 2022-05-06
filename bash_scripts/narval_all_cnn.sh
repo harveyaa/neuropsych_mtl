57e649d57114bdf858a4ff7f18ebbe48a10d72df
Annabelle Harvey <annabelle.ahrv@gmail.com>
2022-05-06 10:26:37 -0400

    added bash scripts

---
+++ bash_scripts/narval_all_cnn.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=all_CNN
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=annabelle.ahrv@gmail.com
+#SBATCH --time=5:00:00
+#SBATCH --mem=5G
+#SBATCH --account=def-pbellec
+#SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/cnn_all.out
+
+tasks='SZ ASD BIP DEL22q11_2 DUP22q11_2 DEL16p11_2 DUP16p11_2 DEL1q21_1 DUP1q21_1'
+
+# DATA PATH
+data_dir='/home/harveyaa/projects/def-pbellec/harveyaa/data/'
+id_dir='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/datasets/cv_folds/hybrid/'
+
+# OUT PATH
+p_out_parent='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/results/all_CNN/'
+
+# SCRIPT
+hps_balanced='/home/harveyaa/projects/def-pbellec/harveyaa/miniMTL/examples/hps_balanced.py'
+
+source /home/harveyaa/projects/def-pbellec/harveyaa/mtl_env/bin/activate
+
+echo 'All tasks MLP'
+for fold in 0 1 2 3 4
+do
+    p_out=$p_out_parent'fold_'$fold
+    echo $p_out
+    mkdir $p_out
+
+    python $hps_balanced --tasks $tasks --num_epochs 100 --batch_size 8 --encoder 0 --head 0 --data_format 1 --log_dir $p_out --id_dir $id_dir --data_dir $data_dir --fold $fold    
+done
