022c6a8f4cad6e3973070d05a988619836b1db43
Annabelle Harvey <annabelle.ahrv@gmail.com>
2022-05-06 16:55:01 -0400

    new bash scripts

--- bash_scripts/narval_all_mlp.sh
+++ bash_scripts/narval_all_mlp.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=all_MLP
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=annabelle.ahrv@gmail.com
-#SBATCH --time=5:00:00
+#SBATCH --time=15:00:00
 #SBATCH --mem=5G
 #SBATCH --account=def-pbellec
 #SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/mlp_all.out
