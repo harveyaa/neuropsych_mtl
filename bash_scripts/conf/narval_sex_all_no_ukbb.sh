312f129d594a0830b4130a5264992fdfc2d4cfc7
Annabelle Harvey <annabelle.ahrv@gmail.com>
2023-02-07 08:49:21 -0500

    added missing bash scripts

---
+++ bash_scripts/conf/narval_sex_all_no_ukbb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=sex_all_no_ukbb
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=annabelle.ahrv@gmail.com
+#SBATCH --time=20:00:00
+#SBATCH --mem=5G
+#SBATCH --account=def-pbellec
+#SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/sex_all.out
+
+#['ds000030', 'Cardiff', 'UKBB', 'BC', 'UCLA', 'SFARI', 'ABIDE','Orban', 'ADHD200', 'ABIDE2']
+tasks='ds000030 Cardiff BC UCLA SFARI Orban ADHD200'
+
+# DATA PATH
+data_dir='/home/harveyaa/projects/def-pbellec/harveyaa/data/'
+
+# OUT PATH
+p_out_parent='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/results/conf/sex/all_no_ukbb/'
+
+# SCRIPT
+hps_conf='/home/harveyaa/projects/def-pbellec/harveyaa/miniMTL/examples/hps_conf.py'
+
+source /home/harveyaa/projects/def-pbellec/harveyaa/mtl_env/bin/activate
+
+echo 'All tasks MLP on conn predicting SEX'
+for fold in 0 1 2 3 4
+do
+    p_out=$p_out_parent'fold_'$fold
+    echo $p_out
+    mkdir $p_out
+   
+    python $hps_conf --tasks $tasks --type 'conn' --conf 'SEX' --n_subsamp 1000 --num_epochs 100 --batch_size 8 --encoder 3 --head 3 --data_format 0 --log_dir $p_out --data_dir $data_dir
+done
---
+++ bash_scripts/conf/narval_sex_single.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=single_sex
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=annabelle.ahrv@gmail.com
+#SBATCH --time=5:00:00
+#SBATCH --mem=5G
+#SBATCH --account=def-pbellec
+#SBATCH --array=0-7
+#SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/single_sex_%a.out
+
+#['ds000030', 'Cardiff', 'UKBB', 'BC', 'UCLA', 'SFARI', 'ABIDE','Orban', 'ADHD200', 'ABIDE2']
+TASK_ARRAY=('ds000030' 'Cardiff' 'UKBB' 'BC' 'UCLA' 'SFARI' 'Orban' 'ADHD200')
+task=${TASK_ARRAY[$SLURM_ARRAY_TASK_ID]}
+
+# DATA PATH
+data_dir='/home/harveyaa/projects/def-pbellec/harveyaa/data/'
+
+# OUT PATH
+p_out_parent='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/results/conf/sex/'
+
+# SCRIPT
+hps_conf='/home/harveyaa/projects/def-pbellec/harveyaa/miniMTL/examples/hps_conf.py'
+
+source /home/harveyaa/projects/def-pbellec/harveyaa/mtl_env/bin/activate
+
+echo 'Single task MLP on conn predicting SEX in '$task
+mkdir $p_out_parent$task
+for fold in 0 1 2 3 4
+do
+    p_out=$p_out_parent$task'/fold_'$fold
+    echo $p_out
+    mkdir $p_out
+
+    python $hps_conf --tasks $task --type 'conn' --conf 'SEX' --n_subsamp 1000 --num_epochs 100 --batch_size 8 --encoder 3 --head 3 --data_format 0 --log_dir $p_out --data_dir $data_dir
+done
