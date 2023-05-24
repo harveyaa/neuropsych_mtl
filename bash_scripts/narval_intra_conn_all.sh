27fbdc895b6372e389c26d36095ebcb943f374c4
Annabelle Harvey <harveyaa@foam.local>
2023-05-24 13:27:00 -0400

    new bash scripts for intrasite cv conditions

---
+++ bash_scripts/narval_intra_conn_all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=all_conn_intrasite
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=annabelle.ahrv@gmail.com
+#SBATCH --time=20:00:00
+#SBATCH --mem=5G
+#SBATCH --account=rrg-jacquese
+#SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/all_conn_intrasite.out
+
+# CNVs & PSYCH STUDY - MTL CONDITIONS - INTRASITE CV
+
+tasks='SZ ASD BIP DEL22q11_2 DUP22q11_2 DEL16p11_2 DUP16p11_2 DEL1q21_1 DUP1q21_1'
+
+# DATA PATH
+data_dir='/home/harveyaa/projects/def-pbellec/harveyaa/data/'
+id_dir='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/datasets/cv_folds/intrasite/'
+
+# OUT PATH
+p_out_parent='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/results_paper/conditions/all/'
+
+# SCRIPT
+hps_balanced='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/hps_balanced.py'
+
+source /home/harveyaa/projects/def-pbellec/harveyaa/mtl_env/bin/activate
+
+echo 'All tasks MLP'
+for fold in 0 1 2 3 4
+do
+    p_out=$p_out_parent'fold_'$fold
+    echo $p_out
+    mkdir $p_out
+
+    python $hps_balanced --tasks $tasks --type 'conn' --strategy 'balanced' --num_epochs 100 --batch_size 8 --encoder 3 --head 3 --data_format 0 --log_dir $p_out --id_dir $id_dir --data_dir $data_dir --fold $fold    
+done
