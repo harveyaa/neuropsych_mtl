715e6c190f450c2f23b5225ef83bf5d6930c2562
Annabelle Harvey <harveyaa@foam.local>
2023-05-29 17:49:30 -0400

    updated bash scripts

--- bash_scripts/narval_age_ukbb_single.sh
+++ bash_scripts/narval_age_ukbb_single.sh
@@ -2,22 +2,25 @@
 #SBATCH --job-name=age_ukbb_single
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=annabelle.ahrv@gmail.com
-#SBATCH --time=40:00:00
+#SBATCH --time=60:00:00
 #SBATCH --mem=5G
 #SBATCH --account=rrg-jacquese
-#SBATCH --array=0-11
+#SBATCH -array=0-59
 #SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/age_ukbb_single_%a.out
 
 # UKBB LEARNING CURVE STUDY - SINGLE AGE
 
-K_ARRAY=(50 100 200 500 1000 1500 2000 2500 3000 3500 4000 4500)
+K_ARRAY=(50 100 200 500 1000 1500 2000 2500 3000 3500 4000 4500 50 100 200 500 1000 1500 2000 2500 3000 3500 4000 4500 50 100 200 500 1000 1500 2000 2500 3000 3500 4000 4500 50 100 200 500 1000 1500 2000 2500 3000 3500 4000 4500 50 100 200 500 1000 1500 2000 2500 3000 3500 4000 4500)
 k=${K_ARRAY[$SLURM_ARRAY_TASK_ID]}
+
+FOLD_ARRAY=(0 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4)
+fold=${FOLD_ARRAY[$SLURM_ARRAY_TASK_ID]}
 
 # DATA PATH
 data_dir='/home/harveyaa/projects/def-pbellec/harveyaa/data/'
 
 # OUT PATH
-p_out_parent='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/results_paper16/age/ukbb/single/'
+p_out_parent='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/results_paper/age/ukbb/single/'
 
 # SCRIPT
 hps_conf='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/hps_conf.py'
@@ -29,12 +32,11 @@ mkdir $p_out_parent$k
 for site in 'UKBB11025' 'UKBB11026' 'UKBB11027'
 do
     mkdir $p_out_parent$k'/'$site
-    for fold in 0 1 2 3 4
-    do
-        p_out=$p_out_parent$k'/'$site'/fold_'$fold
-        echo $p_out
-        mkdir $p_out
+
+    p_out=$p_out_parent$k'/'$site'/fold_'$fold
+    echo $p_out
+    mkdir $p_out
     
-        python $hps_conf --tasks $site --type 'conn' --n_subsamp $k --conf 'AGE' --num_epochs 100 --batch_size 16 --encoder 3 --head 3 --data_format 0 --log_dir $p_out --data_dir $data_dir
-    done
+    python $hps_conf --tasks $site --type 'conn' --n_subsamp $k --conf 'AGE' --num_epochs 100 --batch_size 8 --encoder 3 --head 3 --data_format 0 --fold $fold --log_dir $p_out --data_dir $data_dir
+
 done
