489d17ae6de54d626e0854777d58914d3e8e14f6
Annabelle Harvey <harveyaa@foam.local>
2023-06-28 15:34:55 -0400

    cleanup

---
+++ bash_scripts/disuse/narval_age_all_no_ukbb.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=age_all_no_ukbb
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=annabelle.ahrv@gmail.com
+#SBATCH --time=20:00:00
+#SBATCH --mem=5G
+#SBATCH --account=def-pbellec
+#SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/age_all.out
+
+# SIMPLE TARGETS STUDY - MTL AGE - NO UKBB SENSITIVITY ANALYSIS
+
+# sites with at least 30 controls
+# Exclude UKBB
+tasks='ADHD1 ADHD3 ADHD5 ADHD6 HSJ NYU SZ1 SZ2 SZ3 SZ6 Svip1 Svip2 UCLA_CB UCLA_DS1 USM'
+
+# DATA PATH
+data_dir='/home/harveyaa/projects/def-pbellec/harveyaa/data/'
+
+# OUT PATH
+p_out_parent='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/results/age/all_no_ukbb/'
+
+# SCRIPT
+hps_conf='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/hps_conf.py'
+
+source /home/harveyaa/projects/def-pbellec/harveyaa/mtl_env/bin/activate
+
+echo 'All tasks MLP on conn predicting AGE'
+for fold in 0 1 2 3 4
+do
+    p_out=$p_out_parent'fold_'$fold
+    echo $p_out
+    mkdir $p_out
+
+    python $hps_conf --tasks $tasks --type 'conn' --n_subsamp 1000 --conf 'AGE' --num_epochs 100 --batch_size 8 --encoder 3 --head 33 --data_format 0 --log_dir $p_out --data_dir $data_dir
+done
---
+++ bash_scripts/disuse/narval_bal_concat_single.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=single_concat_bal_test
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=annabelle.ahrv@gmail.com
+#SBATCH --time=5:00:00
+#SBATCH --mem=5G
+#SBATCH --account=def-pbellec
+#SBATCH --array=0-8
+#SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/conn_bal_test_%a.out
+
+# NEGATIVE TRANSFER STUDY - CONCAT BASELINE
+
+TASK_ARRAY=('SZ' 'ASD' 'BIP' 'DEL22q11_2' 'DUP22q11_2' 'DEL16p11_2' 'DUP16p11_2' 'DEL1q21_1' 'DUP1q21_1')
+task=${TASK_ARRAY[$SLURM_ARRAY_TASK_ID]}
+
+# DATA PATH
+data_dir='/home/harveyaa/projects/def-pbellec/harveyaa/data/'
+id_dir='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/datasets/cv_folds/hybrid/'
+
+# OUT PATH
+p_out_parent='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/results/neg_transfer/concat/single/'
+
+# SCRIPT
+hps_balanced='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/hps_balanced.py'
+
+source /home/harveyaa/projects/def-pbellec/harveyaa/mtl_env/bin/activate
+
+echo 'Single task MLP on concat w/ balanced test sets on '$task
+mkdir $p_out_parent$task
+for fold in 0 1 2 3 4
+do
+    p_out=$p_out_parent$task'/fold_'$fold
+    echo $p_out
+    mkdir $p_out
+
+    python $hps_balanced --tasks $task --type 'concat' --strategy 'balanced' --num_epochs 100 --batch_size 8 --encoder 5 --head 5 --data_format 0 --log_dir $p_out --id_dir $id_dir --data_dir $data_dir --fold $fold
+done
