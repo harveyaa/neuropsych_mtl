da57ead8ffd93ed86cd6103d790e4b23fdd87119
Annabelle Harvey <harveyaa@foam.local>
2023-07-04 13:15:23 -0400

    only missing fold for concat

--- bash_scripts/mtl/narval_intra_concat.sh
+++ bash_scripts/mtl/narval_intra_concat.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=all_concat_intra
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=annabelle.ahrv@gmail.com
-#SBATCH --time=40:00:00
+#SBATCH --time=30:00:00
 #SBATCH --mem=5G
 #SBATCH --account=def-pbellec
 #SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/concat_intrasite.out
@@ -24,7 +24,8 @@ hps_balanced='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/hp
 source /home/harveyaa/projects/def-pbellec/harveyaa/mtl_env/bin/activate
 
 echo 'All tasks MLP'
-for fold in 0 1 2 3 4
+#for fold in 0 1 2 3 4
+for fold in 4
 do
     p_out=$p_out_parent'fold_'$fold
     echo $p_out
