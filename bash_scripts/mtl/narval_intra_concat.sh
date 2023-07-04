9c10a650ecce989b77e15cbd0dd57885c9908ed0
Annabelle Harvey <harveyaa@foam.local>
2023-07-04 13:13:54 -0400

    more time for concat mtl

--- bash_scripts/mtl/narval_intra_concat.sh
+++ bash_scripts/mtl/narval_intra_concat.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=all_concat_intra
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=annabelle.ahrv@gmail.com
-#SBATCH --time=30:00:00
+#SBATCH --time=40:00:00
 #SBATCH --mem=5G
 #SBATCH --account=def-pbellec
 #SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/concat_intrasite.out
