08f1d38f4c2a0701bae09f0ddd3686640736a49e
Annabelle Harvey <harveyaa@foam.local>
2023-08-21 17:22:35 -0400

    mmoe3 bash script

---
+++ bash_scripts/mtl/narval_intra_mmoe_3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=mmoe_3
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=annabelle.ahrv@gmail.com
+#SBATCH --time=30:00:00
+#SBATCH --mem=5G
+#SBATCH --account=def-pbellec
+#SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/mmoe_3.out
+
+# CNVs & PSYCH STUDY - MTL CONDITIONS - MMOE w/ 3 EXPERTS
+
+tasks='SZ ASD BIP ADHD DEL15q11_2 DEL22q11_2 DUP22q11_2 DEL16p11_2 DUP16p11_2 DEL1q21_1 DUP1q21_1'
+
+# DATA PATH
+data_dir='/home/harveyaa/projects/def-pbellec/harveyaa/data/'
+id_dir='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/datasets/cv_folds/intrasite/'
+
+# OUT PATH
+p_out_parent='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/results_paper/mtl/mmoe_3/'
+
+# SCRIPT
+mmoe='/home/harveyaa/projects/def-pbellec/harveyaa/neuropsych_mtl/MTL/mmoe.py'
+
+source /home/harveyaa/projects/def-pbellec/harveyaa/mtl_env/bin/activate
+
+echo 'All tasks MMOE - 3 experts'
+for fold in 0 1 2 3 4
+do
+    p_out=$p_out_parent'fold_'$fold
+    echo $p_out
+    mkdir $p_out
+
+    python $mmoe --tasks $tasks --type 'conn' --strategy 'balanced' --num_epochs 100 --batch_size 8 --num_experts 3 --encoder 3 --head 3 --data_format 0 --log_dir $p_out --id_dir $id_dir --data_dir $data_dir --fold $fold    
+done
