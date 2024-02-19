333604999db55e8e4c514d384d681133e50a6848
Annabelle Harvey <harveyaa@foam.local>
2024-02-19 13:50:03 -0500

    mod for more time

--- bash_reviews/baseline/narval_intra_conn_single_cnn_64.sh
+++ bash_reviews/baseline/narval_intra_conn_single_cnn_64.sh
@@ -2,15 +2,16 @@
 #SBATCH --job-name=cnn_64_single
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=annabelle.ahrv@gmail.com
-#SBATCH --time=15:00:00
+#SBATCH --time=20:00:00
 #SBATCH --mem=5G
 #SBATCH --account=def-pbellec
-#SBATCH --array=0-10
+#SBATCH --array=0-2
 #SBATCH -o /home/harveyaa/projects/def-pbellec/harveyaa/slurm_output/cnn_64_single_%a.out
 
 # NEGATIVE TRANSFER STUDY - CNN BASELINE - paper version (intra-site CV)
 
-TASK_ARRAY=('SZ' 'ASD' 'BIP' 'DEL22q11_2' 'DUP22q11_2' 'DEL16p11_2' 'DUP16p11_2' 'DEL1q21_1' 'DUP1q21_1' 'ADHD' 'DEL15q11_2')
+#TASK_ARRAY=('SZ' 'ASD' 'BIP' 'DEL22q11_2' 'DUP22q11_2' 'DEL16p11_2' 'DUP16p11_2' 'DEL1q21_1' 'DUP1q21_1' 'ADHD' 'DEL15q11_2')
+TASK_ARRAY=('SZ' 'ASD' 'ADHD')
 task=${TASK_ARRAY[$SLURM_ARRAY_TASK_ID]}
 
 # DATA PATH
@@ -27,7 +28,8 @@ source /home/harveyaa/projects/def-pbellec/harveyaa/mtl_env2/bin/activate
 
 echo 'Single task CNN on conn w/ intrasite CV on '$task
 mkdir $p_out_parent$task
-for fold in 0 1 2 3 4
+#for fold in 0 1 2 3 4
+for fold in 2 3 4
 do
     p_out=$p_out_parent$task'/fold_'$fold
     echo $p_out
